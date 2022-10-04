4180c10ed2c8ec6ae34ed71def2f1b693a52f591
joseromoluque <jose_santiago_romo_m-tsapp1ta2122@cev.com>
2022-10-04 20:04:44 +0200

    Initial Commit

---
+++ trabajo viernes1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trabajo viernes1
+//
+//  Created by José Santiago  Romo Luque on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ trabajo viernes1UITests/trabajo_viernes1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  trabajo_viernes1UITestsLaunchTests.swift
+//  trabajo viernes1UITests
+//
+//  Created by José Santiago  Romo Luque on 4/10/22.
+//
+
+import XCTest
+
+final class trabajo_viernes1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
